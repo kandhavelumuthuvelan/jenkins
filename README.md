18606e976b025bb958623c701b613505c05b31c7
Kandhavelu Muthuvelan <kandhavelu.muthuvelan@aspiresys.com>
2018-02-06 17:25:53 +0530

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 #mail
 # mail
 # buildproject
+# mail
--- summa.java
+++
@@ -1,9 +0,0 @@
-package jenkins;
-
-public class summa {
-	public static void main(String[] args)
-	{
-		System.out.print("hai hello welcome");
-	}
-
-}
